ebd5c9a75d5c55d5904980638920be8b44526c94
DiegoJaramisho <diegojaramillo473@gmail.com>
2022-01-26 08:51:46 -0500

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,7 +63,7 @@
           </div>
 
           <div class="header-buttons">
-            <a href="{{asset('assets/doc/CV_Diego_Fernando_Jaramillo_Duran.pdf')}}"  target="_blank" class="btn btn-primary" download>Download CV</a>
+            <a href="{{asset('assets/img/foto/CV_Diego_Fernando_Jaramillo_Duran.pdf')}}"  target="_blank" class="btn btn-primary" download>Download CV</a>
           </div>
 
           {{-- <div class="copyrights">© 2020 All rights reserved.</div> --}}
